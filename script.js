9f0295a2b4abdc4ec6c289ec48bddbfda1ee053a
Jesse <44911469+JesseTavrod@users.noreply.github.com>
2023-01-24 21:55:16 -0300

    Estilo e seleted
    Adicionando estilo na pagina e select

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-var atividade = document.getElementById('atividade');
-var dtAtv = document.querySelector('#dataAtv');
-var btnCadastrar = document.querySelector('input[type=submit]');
+var atividade = document.getElementById('activity');
+var dtAtv = document.querySelector('#timeActv');
+const btnCadastrar = document.querySelector('input[type=submit]');
+const btnTask = document.querySelectorAll('button');
+
 var indice = 0;
 
 btnCadastrar.addEventListener('click', (e) => {
-    e.preventDefault(); 
+    e.preventDefault()
 
-    let lista = document.getElementById('lista');
-    console.log('teste')
+    let lista = document.getElementById('list')
+    let linha =  document.createElement('tr')
+    let celId = document.createElement('th')
+    celId.innerText = ++indice
 
-    let linha =  document.createElement('tr');
-    
-    let celId = document.createElement('th');
-    celId.innerText = ++indice;
-
-    let celAtv = document.createElement('td');
+    let celAtv = document.createElement('td')
     celAtv.innerText = atividade.value;
 
 
-    let celData = document.createElement('td');
+    let celData = document.createElement('td')
     let dataFormata = new Date(dtAtv.value).toLocaleDateString('pt-br', {timeZone: 'utc'})
-    celData.innerText = dataFormata;
+    celData.innerText = dataFormata
 
-    let celAct = document.createElement('td');
-    let checkAct = document.createElement('input')
-    checkAct.type = 'checkbox';
+    let celAct = document.createElement('td')
+    let checkAct = document.createElement('select')
+
+    array = ['Done', 'Progress'];
+
+    for(let i = 0; i < array.length; ++i){
+        let option = document.createElement('option')
+        option.value = array[i];
+        option.innerHTML = array[i];
+        checkAct.appendChild(option);
+    }
+
     checkAct.id = 'atv'+indice;
-    
 
     celAct.appendChild(checkAct);
     linha.appendChild(celId);
@@ -35,16 +42,17 @@ btnCadastrar.addEventListener('click', (e) => {
     linha.appendChild(celAct);
     lista.appendChild(linha);
 
-    checkAct.addEventListener('change', () => {
-
-        let resposta = confirm('Deseja mesmo???');
-        
-        if(resposta) {
-            linha.style.textDecoration = 'line-through';
-        } else{
-            linha.style.textDecoration = 'none';
-        }
-       
-    });
 });
 
+btnTask.forEach( button => {
+    button.addEventListener('click', changeTask);
+});
+
+function changeTask(vent){
+    let button = event.target;
+    
+    if(!button.classList.contains("active")){
+        button.classList.add('active');
+    }
+    
+};
