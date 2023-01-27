8a02c72e25a458f05de1d0f7ba395bc45bf42140
Jessé Tavares <jesse@mageda.com.br>
2023-01-26 23:35:36 -0300

    add function changeStatusTask

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 var task = document.getElementById('activity');
 var dtAtv = document.querySelector('#timeActv');
 const btnCadastrar = document.querySelector('input[type=submit]');
-const btnTask = document.querySelectorAll('button');
+const btnTask = document.querySelectorAll('.options-bar button');
+const selectStatus = document.querySelectorAll('.selectStatus');
+
 var arrayNew;
 var indice = 0;
 
 createList('open')
 
-function addRow( name, time, act){
-    
+function addRow( name, time, act, index){
     let allListTask = document.getElementById('list')
     let newRow =  document.createElement('tr')
+    newRow.setAttribute("id", `taskElement-${index}`);
     let celId = document.createElement('th')
     let celAtv = document.createElement('td')
     let celData = document.createElement('td')
 
-    celId.innerText = ++indice
+    celId.innerText = index;
     celAtv.innerText = name;
     celData.innerText = time;
 
     let celAct = document.createElement('td')
     let checkAct = document.createElement('select')
+    checkAct.classList.add("selectStatus");
 
-    array = ['Open','Done', 'Progress'];
+    checkAct.addEventListener('change', setStatusTask);
+
+    array = ['Open','Done', 'Progress', 'Remove'];
 
     for(let i = 0; i < array.length; ++i){
         let option = document.createElement('option')
@@ -48,8 +53,12 @@ function createList(){
             let nameTask = tasks[i].task;
             let timeTask= tasks[i].time;
             let act = tasks[i].status;
-            
-            addRow(nameTask, timeTask, act);
+
+            if(act == "open"){
+                addRow(nameTask, timeTask, act, i);
+            }
+
+
         }
     }
 }
@@ -60,16 +69,50 @@ function addToLocalStorege(task, time){
     if (localStorage.hasOwnProperty("listTasks")) {
         tasks = JSON.parse(localStorage.getItem("listTasks"))
     }
-
+    
     let newInfos = {
         task: task,
         time: time,
-        status: 'open'
+        status: 'open',
+        hours: 0,
+        minutes: 0,
+        seconds: 0
     }
 
     tasks.push(newInfos);
     localStorage.setItem("listTasks", JSON.stringify(tasks));
 }
+
+function changeTabStatus(event){
+    let targetButton = event.target.getAttribute('target');
+    let i;
+    
+    for(i = 0; i < btnTask.length; ++i){
+        if(btnTask[i].getAttribute('target') == targetButton){
+            btnTask[i].classList.add('active')
+        } else {
+            btnTask[i].classList.remove('active')
+        }
+    }
+};
+
+function getListStorage(){
+    return JSON.parse(localStorage.getItem("listTasks"));
+}
+
+function setStatusTask(event){
+    let item = event.target;
+    let row = item.closest('tr').getAttribute('id');
+    let idRow = Number(row.replace("taskElement-", ""));
+    let list = document.querySelectorAll('#list tr');
+    list[idRow].remove();
+    console.log(item.value)
+    list = getListStorage();
+    list[idRow].status = item.value;
+    localStorage.setItem("listTasks", JSON.stringify(list));
+
+}
+
 
 btnCadastrar.addEventListener('click', (e) => {
     e.preventDefault()
@@ -81,21 +124,7 @@ btnCadastrar.addEventListener('click', (e) => {
     addRow(nameTask, newDate, act); 
 });
 
-btnTask.forEach( button => {
-    button.addEventListener('click', changeTask);
-});
 
-function changeTask(event){
-    let targetButton = event.target.getAttribute('target');
-    let i;
-    
-    for(i = 0; i < btnTask.length; ++i){
-        if(btnTask[i].getAttribute('target') == targetButton){
-            btnTask[i].classList.add('active')
-        } else {
-            btnTask[i].classList.remove('active')
-        }
-    }
-
-
-};
+for(let btn = 0; btn < btnTask.length; ++btn){
+    btnTask[btn].addEventListener('click', changeTabStatus);
+}
