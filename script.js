87a3d2f6bf3df805d8e65e76aad82928cd837e5f
Jesse <44911469+JesseTavrod@users.noreply.github.com>
2023-01-28 02:12:16 -0300

    Add changeTabStatus

--- script.js
+++ script.js
@@ -3,36 +3,38 @@ var dtAtv = document.querySelector('#timeActv');
 const btnCadastrar = document.querySelector('input[type=submit]');
 const btnTask = document.querySelectorAll('.options-bar button');
 const selectStatus = document.querySelectorAll('.selectStatus');
+const listTasks = document.querySelector('#list');
 
 var arrayNew;
 var indice = 0;
 
-createList('open')
+createList('Open')
 
-function addRow( name, time, act, index){
-    let allListTask = document.getElementById('list')
+function addRow( name, time, status, index, id){
+
     let newRow =  document.createElement('tr')
-    newRow.setAttribute("id", `taskElement-${index}`);
+    newRow.setAttribute("id", `taskElement-${id}`);
     let celId = document.createElement('th')
     let celAtv = document.createElement('td')
     let celData = document.createElement('td')
 
-    celId.innerText = index;
+    celId.innerText = index != "undefined" ? index : allListTask.childNodes.lenght + 1;
     celAtv.innerText = name;
     celData.innerText = time;
 
     let celAct = document.createElement('td')
     let checkAct = document.createElement('select')
     checkAct.classList.add("selectStatus");
-
-    checkAct.addEventListener('change', setStatusTask);
-
+    checkAct.addEventListener('change', changeStatusTask);
     array = ['Open','Done', 'Progress', 'Remove'];
 
     for(let i = 0; i < array.length; ++i){
         let option = document.createElement('option')
         option.value = array[i];
         option.innerHTML = array[i];
+        if(array[i] == status){
+            option.selected = true;
+        }
         checkAct.appendChild(option);
     }
 
@@ -41,25 +43,27 @@ function addRow( name, time, act, index){
     newRow.appendChild(celData);
     newRow.appendChild(celAct);
     newRow.appendChild(checkAct);
-    allListTask.appendChild(newRow);
+    listTasks.appendChild(newRow);
 
 }
 
-function createList(){
+function createList(type){
+    listTasks.innerHTML = '';
+
     if (localStorage.hasOwnProperty("listTasks")) {
         let tasks = JSON.parse(localStorage.getItem("listTasks"));
+        let filterTasks = tasks.filter( task => task.status == type);
 
-        for(let i = 0; i < tasks.length; ++i){
-            let nameTask = tasks[i].task;
-            let timeTask= tasks[i].time;
-            let act = tasks[i].status;
+        for(let i = 0; i < filterTasks.length; ++i){
+            let nameTask = filterTasks[i].task;
+            let timeTask= filterTasks[i].time;
+            let status = filterTasks[i].status;
+            let id = filterTasks[i].id;
 
-            if(act == "open"){
-                addRow(nameTask, timeTask, act, i);
-            }
-
-
+            addRow(nameTask, timeTask, status, i, id);
         }
+    } else{
+        localStorage.setItem("listTasks", {});
     }
 }
 
@@ -68,9 +72,13 @@ function addToLocalStorege(task, time){
 
     if (localStorage.hasOwnProperty("listTasks")) {
         tasks = JSON.parse(localStorage.getItem("listTasks"))
+        id = Number(tasks[tasks.length - 1].id) + 1;
+    } else {
+        id = 1;
     }
-    
+
     let newInfos = {
+        id: id,
         task: task,
         time: time,
         status: 'open',
@@ -85,8 +93,9 @@ function addToLocalStorege(task, time){
 
 function changeTabStatus(event){
     let targetButton = event.target.getAttribute('target');
+
     let i;
-    
+
     for(i = 0; i < btnTask.length; ++i){
         if(btnTask[i].getAttribute('target') == targetButton){
             btnTask[i].classList.add('active')
@@ -94,25 +103,32 @@ function changeTabStatus(event){
             btnTask[i].classList.remove('active')
         }
     }
+
+    createList(targetButton);
 };
+
 
 function getListStorage(){
     return JSON.parse(localStorage.getItem("listTasks"));
 }
-
-function setStatusTask(event){
-    let item = event.target;
-    let row = item.closest('tr').getAttribute('id');
-    let idRow = Number(row.replace("taskElement-", ""));
-    let list = document.querySelectorAll('#list tr');
-    list[idRow].remove();
-    console.log(item.value)
-    list = getListStorage();
-    list[idRow].status = item.value;
-    localStorage.setItem("listTasks", JSON.stringify(list));
-
+function setListStorage(idElement, status){
+    let newList = getListStorage();
+    let index = newList.findIndex(element => element.id == idElement);
+    newList[index].status = status;
+    localStorage.setItem("listTasks", JSON.stringify(newList));
 }
 
+function changeStatusTask(event){
+    let item = event.target;
+    let row = item.closest('tr').getAttribute('id');
+    let idElement = Number(row.replace("taskElement-", ""));
+    let status = item.value;
+
+    setListStorage(idElement, status);
+
+    createList('open');
+
+}
 
 btnCadastrar.addEventListener('click', (e) => {
     e.preventDefault()
@@ -123,7 +139,6 @@ btnCadastrar.addEventListener('click', (e) => {
     addToLocalStorege( task.value, newDate, act); 
     addRow(nameTask, newDate, act); 
 });
-
 
 for(let btn = 0; btn < btnTask.length; ++btn){
     btnTask[btn].addEventListener('click', changeTabStatus);
